2ac921b332fc16aa2d12fe829263cc30dafacf64
ffalt <ffalty@gmail.com>
2018-12-01 13:43:32 +0100

    build(release): remove dist folder from development branch

--- dist/lib-esm/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { IXLSXExtractOptions } from './types';
-import * as events from 'events';
-export declare class XLSX extends events.EventEmitter {
-    constructor();
-    extract(filename: string, options?: IXLSXExtractOptions): XLSX;
-    convert(filename: string, destfile: string, options: IXLSXExtractOptions): XLSX;
-}
