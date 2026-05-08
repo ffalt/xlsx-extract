d2cb7d97102256992361a069fb9b9006c4f252b7
ffalt <ffalty@gmail.com>
2026-05-08 17:33:31 +0200

    refactor: switch Jest config to CommonJS

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,14 @@
-export default {
+module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	coverageDirectory: "coverage",
 	setupFilesAfterEnv: ["jest-expect-message"],
 	modulePathIgnorePatterns: ["<rootDir>/local/", "<rootDir>/coverage/", "<rootDir>/data", "<rootDir>/dist"],
 	testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(test).[jt]s?(x)"],
-	globals: {}
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1"
+	},
+	transform: {
+		"^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }]
+	}
 };
